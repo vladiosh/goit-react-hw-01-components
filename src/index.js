72049b3b20bcee58cb7d96a37b49cd287080ae2c
VLADYSLAV FEDCHENKO <vladfedcenko33@gmail.com>
2022-11-25 14:48:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import user from '../src/user';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const profile = (
+  <div class="profile">
+    <div class="description">
+      <img
+        src={user.avatar}
+        alt="User avatar"
+        class="avatar"
+        width="150"
+        height="150"
+      />
+      <p class="name">{user.username}</p>
+      <p class="tag">{user.tag}</p>
+      <p class="location">{user.location}</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">{user.stats.followers}</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">{user.stats.views}</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
 );
+
+ReactDOM.createRoot(document.getElementById('root')).render(profile);
+
+//   // <React.StrictMode>
+//   //   <App />
+//   // </React.StrictMode>
